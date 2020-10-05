ad886a1824d0cce218dea5ad4da0bbeed954a337
luis <luishm2024@gmail.com>
2020-10-05 14:29:09 -0500

    first assigment

---
+++ components/AboutHooks.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Text, View, FlatList } from "react-native";
+import { Card } from "react-native-elements";
+import { LEADERS } from "../shared/leaders";
+import { ListItem } from "react-native-elements";
+
+const About = () => {
+  const [leaders, setleaders] = useState(LEADERS);
+
+  const navigationOptions = {
+    title: "About",
+  };
+
+  const renderMenuItem = ({ item, index }) => {
+    return (
+      <ListItem
+        key={index}
+        title={item.name}
+        subtitle={item.designation}
+        hideChevron={true}
+        //  onPress={() => navigate('Dishdetail',{dishId:item.id})}
+        // leftAvatar={{ source: require('./images/uthappizza.png')}}
+      />
+    );
+  };
+
+  return (
+    <View>
+      <Card title="Our History">
+        <Text>
+          Started in 2010, Ristorante con Fusion quickly established itself as a
+          culinary icon par excellence in Hong Kong. With its unique brand of
+          world fusion cuisine that can be found nowhere else, it enjoys
+          patronage from the A-list clientele in Hong Kong. Featuring four of
+          the best three-star Michelin chefs in the world, you never know what
+          will arrive on your plate the next time you visit us.{"\n"}
+        </Text>
+
+        <Text>
+          The restaurant traces its humble beginnings to The Frying Pan, a
+          successful chain started by our CEO, Mr. Peter Pan, that featured for
+          the first time the world's best cuisines in a pan.
+        </Text>
+      </Card>
+      <Card title="Corporate Leadership">
+        <FlatList
+          data={leaders}
+          renderItem={renderMenuItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </Card>
+    </View>
+  );
+};
+export default About;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,75 +1,128 @@
-import React, { Component } from 'react';
-import Home from './HomeComponent';
-import Menu from './MenuComponent';
-import { View,Platform} from 'react-native';
-import Dishdetail from './DishdetailComponent';
-import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
+import React, { Component } from "react";
+import Home from "./HomeComponent";
+import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+import { View, Platform } from "react-native";
+import Dishdetail from "./DishdetailComponent";
+import { createDrawerNavigator, createStackNavigator } from "react-navigation";
 
-
-const MenuNavigator=createStackNavigator(
-    {
-        Menu:{screen:Menu},
-        Dishdetail:{screen:Dishdetail}
+const MenuNavigator = createStackNavigator(
+  {
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail },
+  },
+  {
+    initialRouteName: "Menu",
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
     },
-    {
-        initialRouteName:'Menu',
-        navigationOptions:{
-            headerStyle:{
-                backgroundColor:'#512DA8'
-            },
-            headerTintColor:"#fff",
-            headerTitleStyle:{
-                color:'#fff'
-            }
-        }
-    }
+  }
 );
-const HomeNavigator=createStackNavigator(
-    {
-        Home:{screen:Home}
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
     },
-    {
-        navigationOptions:{
-            headerStyle:{
-                backgroundColor:'#512DA8'
-            },
-            headerTintColor:"#fff",
-            headerTitleStyle:{
-                color:'#fff'
-            }
-        }
-    }
+  }
 );
-const MainNavigator=createDrawerNavigator({
-    Home:{
-        screen:HomeNavigator,
-        navigationOptions:{
-            title:'Home',
-            drawerLabel:'Home'
-        }
+const ContactNavigator = createStackNavigator(
+  {
+    Contact: { screen: Contact },
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
     },
-    Menu:{
-        screen:MenuNavigator,
-        navigationOptions:{
-            title:'Menu',
-            drawerLabel:'Menu'
-        },
-    }
-},{
-    drawerBackgroundColor:'#D1C4E9'
-});
+  }
+);
+const AboutNavigator = createStackNavigator(
+  {
+    About: { screen: About },
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        title: "Home",
+        drawerLabel: "Home",
+      },
+    },
+    Menu: {
+      screen: MenuNavigator,
+      navigationOptions: {
+        title: "Menu",
+        drawerLabel: "Menu",
+      },
+    },
+    Contact: {
+      screen: ContactNavigator,
+      navigationOptions: {
+        title: "Contat Us",
+        drawerLabel: "Contact Us",
+      },
+    },
+    About: {
+      screen: AboutNavigator,
+      navigationOptions: {
+        title: "About US",
+        drawerLabel: "About US",
+      },
+    },
+  },
+  {
+    drawerBackgroundColor: "#D1C4E9",
+  }
+);
 
 class Main extends Component {
-   
- 
   render() {
- 
     return (
-        <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+      <View
+        style={{
+          flex: 1,
+          paddingTop:
+            Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+        }}
+      >
         <MainNavigator />
-         </View>
+      </View>
     );
   }
 }
-  
+
 export default Main;
